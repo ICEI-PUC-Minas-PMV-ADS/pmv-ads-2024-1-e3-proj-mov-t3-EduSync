938b3d9d268dbbd906ef29853b4660be1dcfd0d7
rafaelcassiano <rafael.souza.1453509@sga.pucminas.br>
2024-06-20 16:25:15 -0300

    arquivos package

--- EduSync/babel.config.js
+++ EduSync/babel.config.js
@@ -2,5 +2,17 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'], // Defina a raiz como a pasta 'src'
+          alias: {
+            '@services': './Service', // Corrija o caminho relativo ao root
+            // adicione mais aliases conforme necessário
+          },
+        },
+      ],
+    ],
   };
 };
